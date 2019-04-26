53806ceea4f1ef2acc37360d08e3d35c5f3110cc
Santosh Dulal <milestonesd@yahoo.co.uk>
2019-04-26 15:20:08 +0545

    votes

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,62 @@
+@extends('adminlte::page')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <div class="box-title">
+                    {{__('View all Users')}}
+                </div>
+            </div>
+            <div class="box-body">
+                <table class="table table-primary table table-bordered">
+                    <thead>
+                        <tr>
+                           
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Post</th>
+                            <th>Department</th>
+                            <th>Photo</th>
+                            <th>Type</th>
+                            <th>Bio</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{$user->id}}</td>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->email}}</td>
+                                <td>{{$user->post}}</td>
+                                <td>{{$user->department}}</td>
+                                <td>{{$user->photo}}</td>
+                                <td>{{$user->type}}</td>
+                                <td>{{$user->bio}}</td>
+                                    <td>
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-default">Action</button>
+                                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                                                <span class="caret"></span>
+                                                <span class="sr-only">Toggle Dropdown</span>
+                                            </button>
+                                            <ul class="dropdown-menu" role="menu">
+                                                <li><a href="{{route('users.edit',[$user->id])}}">Edit</a></li>
+                                                <li><a href="#">View</a></li>
+                                                <li><a href="{{route('users.destroy',[$user->id])}}">Delete</a></li>
+                                             </ul>
+                            </div>
+                                    </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+    </div>
+</div>
+    
+@endsection
